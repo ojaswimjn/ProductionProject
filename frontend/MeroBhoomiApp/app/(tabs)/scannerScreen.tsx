cb8aa3f035343c80df5c13feaea7b4cc34d20f10
ojaswimjn <maharjanojaswi573@gmail.com>
2025-03-29 23:14:14 +0545

    initial camera installed

--- frontend/MeroBhoomiApp/app/(tabs)/scannerScreen.tsx
+++ frontend/MeroBhoomiApp/app/(tabs)/scannerScreen.tsx
@@ -1,18 +1,19 @@
 import { useState } from 'react';
-import { Button, Image, View, StyleSheet } from 'react-native';
+import { Button, Image, View, StyleSheet ,Text, TouchableOpacity,  } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 
 export default function ImagePickerExample() {
   const [image, setImage] = useState<string | null>(null);
-  
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images', 'videos'],
       allowsEditing: true,
-      aspect: [6, 4],
+      aspect: [4, 3],
       quality: 1,
     });
 
@@ -23,10 +24,50 @@ export default function ImagePickerExample() {
     }
   };
 
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  const openCamera = async( ) => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing:true,
+      aspect:[4,3],
+      quality:1,
+    })
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
+      <Button title="Open camera" onPress={openCamera} />
       {image && <Image source={{ uri: image }} style={styles.image} />}
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
@@ -40,5 +81,28 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
